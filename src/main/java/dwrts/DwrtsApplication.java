90ed1ca91f2e991af88190d007f60903c2b6ada5
l0bbyist <johnkilobytes@gmail.com>
2025-07-30 13:38:58 +0300

    First Commit

---
+++ src/main/java/dwrts/DwrtsApplication.java
@@ -0,0 +1,13 @@
+package dwrts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwrtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwrtsApplication.class, args);
+	}
+
+}
